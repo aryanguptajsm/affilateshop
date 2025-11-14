e4d5e8c45f4cb26d6299dfc3cb4932808d4be2c7
Aryan Gupta <aryangupta.jsm@gmail.com>
2025-11-14 16:09:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-fetch("https://YOUR_BACKEND_URL/products")
+const backend = "https://affilateshop-backend.onrender.com";
+
+fetch(backend + "/products")
   .then(res => res.json())
   .then(products => {
     let html = "";
     products.forEach(p => {
+
+      const imageURL = backend + "/" + p.image; 
+      // example → https://affilateshop-backend.onrender.com/uploads/123-image.jpg
+
       html += `
       <div class="product-card">
-        <img src="${p.image}">
+        <img src="${imageURL}">
         <h3>${p.name}</h3>
         <p>₹${p.price}</p>
         <a class="buy-btn" href="${p.link}" target="_blank">Buy Now</a>
@@ -14,4 +20,5 @@ fetch("https://YOUR_BACKEND_URL/products")
     });
 
     document.getElementById("productList").innerHTML = html;
-  });
+  })
+  .catch(err => console.error("Error:", err));
