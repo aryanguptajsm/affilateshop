164ebcaa273aa4112dc084ff50e24bd30d0ac08d
Aryan gupta <aryangupta.jsm@gmail.com>
2026-08-02 23:12:26 +0530

    Refactor search input selection to improve element querying in script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-const searchBar = document.querySelector(".search-bar");
-const searchInput = searchBar.querySelector("input");
+const controls = document.querySelector(".controls");
+const searchBar = controls.querySelector(".search-bar");
+const searchInput = searchBar.querySelector("searchInput");
 console.log(searchInput);
