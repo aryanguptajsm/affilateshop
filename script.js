b07c585365f9e23098086ddd2cf99afe950869be
Aryan gupta <aryangupta.jsm@gmail.com>
2026-08-03 23:28:22 +0530

    Comment out movie detail assignments in fetch response handling

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ fetch(url)
     .then(response => response.json())
     .then(data => {
 
-        title.textContent = data.Title;
-        year.textContent = data.Year;
-        rating.textContent = data.imdbRating;
-        genre.textContent = data.Genre;
-        runtime.textContent = data.Runtime;
-        director.textContent = data.Director;
-        plot.textContent = data.Plot;
-        poster.src = data.Poster;
+       // title.textContent = data.Title;
+       // year.textContent = data.Year;
+       // rating.textContent = data.imdbRating;
+       // genre.textContent = data.Genre;
+       // runtime.textContent = data.Runtime;
+       // director.textContent = data.Director;
+       //////////// plot.textContent = data.Plot;
+       // poster.src = data.Poster;//
 
     });
