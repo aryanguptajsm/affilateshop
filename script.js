f28a008fde3769b2121cb36bc9848260e2056ccf
Aryan gupta <aryangupta.jsm@gmail.com>
2026-08-03 23:21:14 +0530

    Replace search input event listener with API fetch for movie data

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
  const searchInput = document.querySelector("#searchInput");
 
- searchInput.addEventListener("input", () => {
-    console.log(searchInput.value);
- });
+const url = "https://www.omdbapi.com/?apikey=af3e0d4&t=batman";
+
+fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
