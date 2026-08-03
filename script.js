50aa49f5bc94316f95b4eff098765d00bef5b70e
Aryan gupta <aryangupta.jsm@gmail.com>
2026-08-03 23:22:27 +0530

    Update fetch response handling to display movie details in the UI

--- script.js
+++ script.js
@@ -3,12 +3,16 @@
 const url = "https://www.omdbapi.com/?apikey=af3e0d4&t=batman";
 
 fetch(url)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((error) => {
-        console.log(error);
+    .then(response => response.json())
+    .then(data => {
+
+        title.textContent = data.Title;
+        year.textContent = data.Year;
+        rating.textContent = data.imdbRating;
+        genre.textContent = data.Genre;
+        runtime.textContent = data.Runtime;
+        director.textContent = data.Director;
+        plot.textContent = data.Plot;
+        poster.src = data.Poster;
+
     });
