2ab6a57cb7e3ca3794194d1ecf05fd334d2b218c
Aryan gupta <aryangupta.jsm@gmail.com>
2026-08-04 22:41:25 +0530

    Refactor feature card layout and update API fetch for show details

--- script.js
+++ script.js
@@ -21,26 +21,27 @@ const topRowYear = document.querySelector(".year-tag");
 const metaRow = document.querySelector(".meta-row");
 
 function urlfetch(){
-     const url = `https://www.omdbapi.com/?apikey=af3e0d4&t=${searchInput.value.trim()}`;
+   //  const url = `https://www.omdbapi.com/?apikey=af3e0d4&t=${searchInput.value.trim()}`;
+    const url = `https://api.tvmaze.com/search/shows?q=${searchInput.value.trim()}`;
 
 fetch(url)
     .then(response => response.json())
     .then(data => {
        console.log(data);
+       console.log(data[0].show.name);
 
-       cardTitle.textContent = data.Title;
+       cardTitle.textContent = data[0].show.name;
         topRowYear.textContent = data.Year;
-        topRowRating.textContent = data.imdbRating;
-       detailsTitle.textContent = data.Title; 
+        topRowRating.textContent = data[0].show.rating.average;
+       detailsTitle.textContent = data[0].show.name; 
+      cardGenre.textContent = data[0].show.genres[0];
 
         metaRow.innerHTML = `
-        <span class="rate">★ ${data.imdbRating}</span>
-        <span>${data.Year}</span>
-        <span>${data.Genre}</span>
-        <span>${data.Runtime}</span>
-      `;
-       descText.textContent = data.Plot;
-        featureimg.src = data.Poster;
+        <span class="rate">★ ${data[0].show.rating.average}</span>
+        <span>${data[0].show.premiered}</span>
+        <span>${data[0].show.genres}</span>`;
+       descText.textContent = data[0].show.summary;
+        featureimg.src = data[0].show.image.original;
 
     });         
 }
